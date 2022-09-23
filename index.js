fc786b25d246b65951b2f9393f3bf65c3683ca80
hamzat <lawalhamzat27@gmail.com>
2022-09-23 10:51:03 +0100

    comm

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ if(process.env.NODE_ENV === "production"){
     app.use(express.static("my-app/build"));
     app.get('*',(req,res)=>{
        res.sendFile(path.resolve(__dirname, 'my-app','build','index.html'));
-    })
+    });
 }
 app.listen(port, console.log(`server is listening on localHost ${port}`));
 mongoose.connect(uri,{
