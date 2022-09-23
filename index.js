19169a1e60040a3002239341aa8eec82a98300f8
hamzat <lawalhamzat27@gmail.com>
2022-09-23 10:43:50 +0100

    work

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const path = require('path');
 require("dotenv").config();
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb'}));
-app.use(cors())
+app.use(cors());
 
 app.use("/api/register", register)
 app.use("/api/login",login)
